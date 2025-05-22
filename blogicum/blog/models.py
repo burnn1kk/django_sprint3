706387939ba8d6a61f8573151520de72627eb981
burnn1k <nikburyagin@gmail.com>
2025-05-22 22:37:26 +0300

    fckpep8

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,44 +1,97 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+
 class BaseModel(models.Model):
-    title = models.CharField(verbose_name='Заголовок', max_length=256, null=False)
-    is_published = models.BooleanField(verbose_name='Опубликовано',
-        default=True, null=False,
-        help_text = 'Снимите галочку, чтобы скрыть публикацию.')
-    created_at = models.DateTimeField(verbose_name='Добавлено', auto_now_add=True, null=False)
+    title = models.CharField(
+        verbose_name='Заголовок',
+        max_length=256,
+        null=False
+    )
+    is_published = models.BooleanField(
+        verbose_name='Опубликовано',
+        default=True,
+        null=False,
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
+    )
+    created_at = models.DateTimeField(
+        verbose_name='Добавлено',
+        auto_now_add=True,
+        null=False
+    )
+
     class Meta:
         abstract = True
 
+
 User = get_user_model()
 
+
 class Location(BaseModel):
-    name = models.CharField(verbose_name='Название места', max_length=256, null=False)
+    name = models.CharField(
+        verbose_name='Название места',
+        max_length=256,
+        null=False
+    )
+
     class Meta:
         verbose_name = 'местоположение'
-        verbose_name_plural = 'Местоположения' 
+        verbose_name_plural = 'Местоположения'
+
+
 class Category(BaseModel):
-    description = models.TextField(verbose_name='Описание', null=False)
-    slug = models.SlugField(verbose_name='Идентификатор',
+    description = models.TextField(
+        verbose_name='Описание',
+        null=False
+    )
+    slug = models.SlugField(
+        verbose_name='Идентификатор',
         unique=True,
         null=False,
-        help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.')
+        help_text=(
+            'Идентификатор страницы для URL; разрешены символы '
+            'латиницы, цифры, дефис и подчёркивание.'
+        )
+    )
+
     class Meta:
         verbose_name = 'категория'
-        verbose_name_plural = 'Категории' 
+        verbose_name_plural = 'Категории'
+
 
 class Post(BaseModel):
-    text = models.TextField(verbose_name='Текст', null=False)
-    pub_date = models.DateTimeField(verbose_name='Дата и время публикации',
+    text = models.TextField(
+        verbose_name='Текст',
+        null=False
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время публикации',
         null=False,
-        help_text='Если установить дату и время в будущем — можно делать отложенные публикации.')
-    author = models.ForeignKey(User, verbose_name='Автор публикации', on_delete=models.CASCADE, null=False)
-    location = models.ForeignKey(Location, verbose_name='Местоположение', on_delete=models.SET_NULL, null=True)
-    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.SET_NULL, blank=False, null=True)
+        help_text=(
+            'Если установить дату и время в будущем — '
+            'можно делать отложенные публикации.'
+        )
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор публикации',
+        on_delete=models.CASCADE,
+        null=False
+    )
+    location = models.ForeignKey(
+        Location,
+        verbose_name='Местоположение',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категория',
+        on_delete=models.SET_NULL,
+        blank=False,
+        null=True
+    )
+
     class Meta:
         verbose_name = 'публикация'
-        verbose_name_plural = 'Публикации' 
-
-
-
-
+        verbose_name_plural = 'Публикации'
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Category
 from django.utils import timezone
+
+from .models import Post, Category
 
 
 def index(request):
@@ -16,26 +17,31 @@ def index(request):
 
 def post_detail(request, id):
     template_name = 'blog/detail.html'
-    post = get_object_or_404(Post.objects.filter(is_published=True,
-        pub_date__lte=timezone.now(),
-        category__is_published=True),
-        id=id)
-
+    post = get_object_or_404(
+        Post.objects.filter(
+            is_published=True,
+            pub_date__lte=timezone.now(),
+            category__is_published=True
+        ),
+        id=id
+    )
     context = {"post": post}
     return render(request, template_name, context)
 
 
 def category_posts(request, category_slug):
     template_name = 'blog/category.html'
-    category = get_object_or_404(Category,
+    category = get_object_or_404(
+        Category,
         slug=category_slug,
-        is_published=True)
-    posts = Post.objects.filter(category=category,
+        is_published=True
+    )
+    posts = Post.objects.filter(
+        category=category,
         is_published=True,
-        pub_date__lte=timezone.now()    
+        pub_date__lte=timezone.now()
     ).order_by('-pub_date')
     context = {
-
         'category': category,
         'post_list': posts,
     }
